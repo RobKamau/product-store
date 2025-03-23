21e125f034bc56e29218f58da7bf5c202fbcc806
RobKamau <rkamaufelix@gmail.com>
2025-03-23 19:12:22 -0400

    app.module.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSubject.asObservable();
 
-  constructor() { }
+  // Set a user as logged in
+  login(user: User): void {
+    this.currentUserSubject.next(user);
+  }
+
+  // Log the user out
+  logout(): void {
+    this.currentUserSubject.next(null);
+  }
+
+  // Get the current logged-in user
+  getCurrentUser(): User | null {
+    return this.currentUserSubject.getValue();
+  }
 }
